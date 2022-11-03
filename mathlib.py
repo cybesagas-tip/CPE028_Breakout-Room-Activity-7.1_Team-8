d156dc3393ecfc7543e8acc68a3bc60739ffdbdf
cybesagas-tip <qciabesagas@tip.edu.ph>
2022-11-03 11:44:33 +0800

    Create mathlib.py

---
+++ mathlib.py
@@ -0,0 +1,24 @@
+def calc_addition(a, b):
+    return a + b
+ 
+ 
+def calc_multiply(a, b):
+    return a * b
+     
+ 
+def calc_substraction(a, b):
+    return a - b
+
+import mathlib
+ 
+def test_calc_addition():
+    output = mathlib.calc_addition(2,4)
+    assert output == 6
+ 
+def test_calc_substraction():
+    output = mathlib.calc_substraction(2, 4)
+    assert output == -2
+     
+def test_calc_multiply():
+    output = mathlib.calc_multiply(2,4)
+    assert output == 8
